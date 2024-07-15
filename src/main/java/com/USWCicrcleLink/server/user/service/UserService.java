e2527d051accb7d9c69b1714d51cf9efac201f79
namgoong <cookie_31@naver.com>
2024-07-15 18:24:05 +0900

    fix: user domain setter 수정

--- src/main/java/com/USWCicrcleLink/server/user/service/UserService.java
+++ src/main/java/com/USWCicrcleLink/server/user/service/UserService.java
@@ -31,8 +31,10 @@ public class UserService {
             throw new IllegalArgumentException("기존 비밀번호가 일치하지 않습니다.");
         }
 
-        user.setUserPw(newPassword);
+        user.updateUserPw(newPassword);
         userRepository.save(user);
+
+        log.info("비밀번호 변경 완료: {}",user.getUserUUID());
     }
 
     // 임시 회원 가입
