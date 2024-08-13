519e362bb581864fb49463b3fc7e80c28cec9657
hanjh <wg1004s@naver.com>
2024-08-13 17:46:24 +0900

    refactor: 이메일 인증 확인 후 html 파일을 리턴하도록 수정

--- src/main/java/com/USWCicrcleLink/server/user/api/UserController.java
+++ src/main/java/com/USWCicrcleLink/server/user/api/UserController.java
@@ -2,6 +2,7 @@ package com.USWCicrcleLink.server.user.api;
 
 import com.USWCicrcleLink.server.email.domain.EmailToken;
 import com.USWCicrcleLink.server.email.service.EmailTokenService;
+import com.USWCicrcleLink.server.global.exception.errortype.EmailException;
 import com.USWCicrcleLink.server.global.response.ApiResponse;
 import com.USWCicrcleLink.server.global.security.dto.TokenDto;
 import com.USWCicrcleLink.server.user.domain.AuthToken;
@@ -17,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.UUID;
 
@@ -39,36 +41,6 @@ public class UserController {
         return new ApiResponse<>("비밀번호가 성공적으로 업데이트 되었습니다.");
     }
 
-    // 임시 회원 등록 및 인증 메일 전송
-    @PostMapping("/temporary")
-    public ResponseEntity<ApiResponse<VerifyEmailResponse>> registerTemporaryUser(@Valid @RequestBody SignUpRequest request)  {
-
-        UserTemp userTemp = userService.registerUserTemp(request);
-        EmailToken emailToken = emailTokenService.createEmailToken(userTemp);
-        userService.sendSignUpMail(userTemp,emailToken);
-
-        ApiResponse<VerifyEmailResponse> verifyEmailResponse = new ApiResponse<>("인증 메일 전송 완료",
-                new VerifyEmailResponse(emailToken.getEmailTokenUUID(), userTemp.getTempAccount()));
-
-        return new ResponseEntity<>(verifyEmailResponse, HttpStatus.OK);
-    }
-
-    // 이메일 인증 확인 후 자동 회원가입
-    @GetMapping("/email/verify-token")
-    public ResponseEntity<ApiResponse<Void>> verifySignUpMail (@RequestParam UUID emailToken_uuid) {
-
-        UserTemp userTemp = userService.verifyEmailToken(emailToken_uuid);
-        userService.signUp(userTemp);
-        return ResponseEntity.ok(new ApiResponse<>("인증이 성공되었습니다. 어플 내에서 회원가입 완료를 눌러주세요"));
-    }
-
-    // 회원 가입 완료 처리
-    @PostMapping("/finish-signup")
-    public ResponseEntity<ApiResponse<String>> signUpFinish(@RequestBody VerifyEmailRequest request) {
-        ApiResponse<String> apiResponse = new ApiResponse<>(userService.signUpFinish(request.getAccount()), "회원가입 완료");
-        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
-    }
-
     // 회원가입 시의 계정 중복 체크
     @GetMapping("/verify-duplicate/{account}")
     public ResponseEntity<ApiResponse<String>> verifyAccountDuplicate(@PathVariable String account) {
@@ -87,6 +59,61 @@ public class UserController {
 
         return ResponseEntity.ok(new ApiResponse<>("비밀번호가 일치합니다"));
     }
+
+    // 임시 회원 등록 및 인증 메일 전송
+    @PostMapping("/temporary")
+    public ResponseEntity<ApiResponse<VerifyEmailResponse>> registerTemporaryUser(@Valid @RequestBody SignUpRequest request)  {
+
+        UserTemp userTemp = userService.registerUserTemp(request);
+        EmailToken emailToken = emailTokenService.createEmailToken(userTemp);
+        userService.sendSignUpMail(userTemp,emailToken);
+
+        ApiResponse<VerifyEmailResponse> verifyEmailResponse = new ApiResponse<>("인증 메일 전송 완료",
+                new VerifyEmailResponse(emailToken.getEmailTokenUUID(), userTemp.getTempAccount()));
+
+        return new ResponseEntity<>(verifyEmailResponse, HttpStatus.OK);
+    }
+
+    // 이메일 인증 확인 후 자동 회원가입
+    @GetMapping("/email/verify-token")
+    public ModelAndView verifySignUpMail (@RequestParam UUID emailToken_uuid) {
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        try {
+            UserTemp userTemp = userService.verifyEmailToken(emailToken_uuid);
+            userService.signUp(userTemp);
+            modelAndView.setViewName("email_verification_success"); // Name of the success page
+            modelAndView.addObject("message", "이메일 인증이 성공했습니다. 앱으로 돌아가 회원가입 완료 버튼을 눌러주세요");
+        } catch (EmailException e) {
+            modelAndView.setViewName("email_verification_failure"); // Name of the failure page
+            modelAndView.addObject("message", "이메일 인증이 실패 했습니다. 이메일을 재전송 해주세요");
+        }
+
+        return modelAndView;
+//        return ResponseEntity.ok(new ApiResponse<>("인증이 성공되었습니다. 어플 내에서 회원가입 완료를 눌러주세요"));
+    }
+
+    // 이메일 재인증
+    @PostMapping("/email/resend-confirmation")
+    public ResponseEntity<ApiResponse<UUID>> resendConfirmEmail(@RequestHeader UUID emailToken_uuid)  {
+
+        EmailToken emailToken = emailTokenService.updateCertificationTime(emailToken_uuid);
+        userService.sendSignUpMail(emailToken.getUserTemp(),emailToken);
+
+        ApiResponse<UUID> response = new ApiResponse<>("이메일 재인증을 해주세요", emailToken_uuid);
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+
+    // 회원 가입 완료 처리
+    @PostMapping("/finish-signup")
+    public ResponseEntity<ApiResponse<String>> signUpFinish(@RequestBody VerifyEmailRequest request) {
+        ApiResponse<String> apiResponse = new ApiResponse<>(userService.signUpFinish(request.getAccount()), "회원가입 완료");
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+
 
     // 로그인
     @PostMapping("/login")
@@ -140,15 +167,6 @@ public class UserController {
         return new ApiResponse<>("비밀번호가 변경되었습니다.");
     }
 
-    // 이메일 재인증
-    @PostMapping("/email/resend-confirmation")
-    public ResponseEntity<ApiResponse<UUID>> resendConfirmEmail(@RequestHeader UUID emailToken_uuid)  {
 
-        EmailToken emailToken = emailTokenService.updateCertificationTime(emailToken_uuid);
-        userService.sendSignUpMail(emailToken.getUserTemp(),emailToken);
-
-        ApiResponse<UUID> response = new ApiResponse<>("이메일 재인증을 해주세요", emailToken_uuid);
-        return new ResponseEntity<>(response,HttpStatus.OK);
-    }
 
 }
