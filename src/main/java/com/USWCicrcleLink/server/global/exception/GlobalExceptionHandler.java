1e43caf5ab53d372eb8396cfbe50caad6b05f61b
hanjh <wg1004s@naver.com>
2024-08-19 22:03:01 +0900

    feat: enum 필드 유효성 검사 기능 및 json parse error 예외처리

--- src/main/java/com/USWCicrcleLink/server/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/USWCicrcleLink/server/global/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.USWCicrcleLink.server.global.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -58,7 +59,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, exceptionType.getStatus());
     }
     
-    //NotBlank 예외 처리
+    // NotBlank 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
@@ -70,4 +71,28 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+
+    // 유효하지 않은 enum값 예외 처리
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String code = "BAD_REQUEST";
+        String className = e.getClass().getName();
+        String message = "해당 필드에서 지원하지 않는 값 입니다";
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .exception(className.substring(className.lastIndexOf(".") + 1))
+                .code(code)
+                .message(message)
+                .status(status.value())
+                .error(status.getReasonPhrase())
+                .build();
+
+        log.error("code : {}, message : {}", errorResponse.getCode(), errorResponse.getMessage());
+
+        return new ResponseEntity<>(errorResponse, status);
+    }
+
+
+
 }
---
+++ src/main/java/com/USWCicrcleLink/server/global/validation/EnumFormat.java
@@ -0,0 +1,20 @@
+package com.USWCicrcleLink.server.global.validation;
+
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {EnumFormatValidator.class})
+public @interface EnumFormat {
+    String message() default "해당 필드 타입에서 지원하지 않는 값 입니다.";
+    Class[] groups() default {};
+    Class[] payload() default {};
+
+    Class<? extends Enum<?>> enumClass();
+
+}
