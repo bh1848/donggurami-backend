e5d6db87f47d22e21dd0dcae994318e2238ea2f3
hanjh <wg1004s@naver.com>
2024-08-19 22:01:42 +0900

    feat: 로그인시 제한 조건 추가

--- src/main/java/com/USWCicrcleLink/server/user/dto/LogInRequest.java
+++ src/main/java/com/USWCicrcleLink/server/user/dto/LogInRequest.java
@@ -20,7 +20,9 @@ public class LogInRequest {
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 영문 대소문자 및 숫자만 가능합니다.",groups = ValidationGroups.PatternGroup.class)
     private String account;
 
-    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.",groups = ValidationGroups.NotBlankGroup.class)
+    @Size(min = 5, max = 20, message = "비밀번호는 5~20자 이내여야 합니다.",groups = ValidationGroups.SizeGroup.class)
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]+$", message = "비밀번호는 영문 대소문자, 숫자, 특수문자만 포함할 수 있습니다.",groups = ValidationGroups.PatternGroup.class)
     private String password;
 
     private String fcmToken;
