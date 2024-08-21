2ff6a8531cfaaf8985b229f420b0c5955161a58e
hanjh <wg1004s@naver.com>
2024-08-21 23:23:31 +0900

    feat: ServerApplication 파일에 @EnableScheduling 추가

--- src/main/java/com/USWCicrcleLink/server/ServerApplication.java
+++ src/main/java/com/USWCicrcleLink/server/ServerApplication.java
@@ -2,19 +2,15 @@ package com.USWCicrcleLink.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.USWCicrcleLink.server")
+@EnableScheduling
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
