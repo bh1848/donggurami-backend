2d11c7b4d81f82adfb7164bc115933ab3fc553e0
hanjh <wg1004s@naver.com>
2024-11-01 19:08:45 +0900

    refactor: 비밀번호 설정시 특수문자 종류 변경

--- src/main/java/com/USWCicrcleLink/server/user/dto/SignUpRequest.java
+++ src/main/java/com/USWCicrcleLink/server/user/dto/SignUpRequest.java
@@ -24,7 +24,7 @@ public class SignUpRequest {
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.",groups = NotBlankGroup.class)
     @Size(min = 5, max = 20, message = "비밀번호는 5~20자 이내여야 합니다.",groups = SizeGroup.class)
-    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]+$", message = "비밀번호는 영문 대소문자, 숫자, 특수문자만 포함할 수 있습니다.",groups = PatternGroup.class)
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()]+$", message = "비밀번호는 영문 대소문자, 숫자, 특수문자만 포함할 수 있습니다.",groups = PatternGroup.class)
     private String password;
 
     @NotBlank(message = "이름 필수 입력 값입니다.")
