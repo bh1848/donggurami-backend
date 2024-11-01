3cbcd2646a948a1e4cee8c73b2e356de85ac91af
namgoong <cookie_31@naver.com>
2024-11-01 22:58:17 +0900

    refactor: update profile regex

--- src/main/java/com/USWCicrcleLink/server/user/dto/UpdatePwRequest.java
+++ src/main/java/com/USWCicrcleLink/server/user/dto/UpdatePwRequest.java
@@ -1,10 +1,19 @@
 package com.USWCicrcleLink.server.user.dto;
 
+import com.USWCicrcleLink.server.global.validation.ValidationGroups;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdatePwRequest {
     private String userPw;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.",groups = ValidationGroups.NotBlankGroup.class)
+    @Size(min = 5, max = 20, message = "비밀번호는 5~20자 이내여야 합니다.",groups = ValidationGroups.SizeGroup.class)
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]+$", message = "비밀번호는 영문 대소문자, 숫자, 특수문자만 포함할 수 있습니다.",groups = ValidationGroups.PatternGroup.class)
     private String newPw;
+
     private String confirmNewPw;
 }
