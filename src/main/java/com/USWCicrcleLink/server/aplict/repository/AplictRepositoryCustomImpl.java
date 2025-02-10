4a880f3a110f443126d16c5f0697649fb1741aac
gioJioh <sjgio0515@naver.com>
2025-02-10 19:04:27 +0900

    refactor: 최초 지원자, 불합격자 조회 페이지네이션 삭제

--- src/main/java/com/USWCicrcleLink/server/aplict/repository/AplictRepositoryCustomImpl.java
+++ src/main/java/com/USWCicrcleLink/server/aplict/repository/AplictRepositoryCustomImpl.java
@@ -4,11 +4,7 @@ import com.USWCicrcleLink.server.aplict.domain.Aplict;
 import com.USWCicrcleLink.server.aplict.domain.AplictStatus;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -30,61 +26,27 @@ public class AplictRepositoryCustomImpl implements AplictRepositoryCustom {
 
     // 동아리 지원자 조회
     @Override
-    public Page<Aplict> findAllWithProfileByClubId(Long clubId, Pageable pageable, boolean checked) {
-        String jpql = "SELECT ap FROM Aplict ap JOIN FETCH ap.profile p" +
-                " WHERE ap.club.id = :clubId AND ap.checked = :checked";
-        TypedQuery<Aplict> query = em.createQuery(jpql, Aplict.class);
-
-        query.setParameter("clubId", clubId);
-        query.setParameter("checked", checked);
-        query.setFirstResult((int) pageable.getOffset());
-        query.setMaxResults(pageable.getPageSize());
-
-        List<Aplict> resultList = query.getResultList();
-
-        long total = getAplictTotalCount(clubId, checked);
-
-        return new PageImpl<>(resultList, pageable, total);
+    public List<Aplict> findAllWithProfileByClubId(Long clubId, boolean checked) {
+        return em.createQuery(
+                        "SELECT ap FROM Aplict ap JOIN FETCH ap.profile p" +
+                                " WHERE ap.club.id = :clubId AND ap.checked = :checked",
+                        Aplict.class
+                ).setParameter("clubId", clubId)
+                .setParameter("checked", checked)
+                .getResultList();
     }
 
     // 불합격자 동아리 지원자 조회
     @Override
-    public Page<Aplict> findAllWithProfileByClubIdAndFailed(Long clubId, Pageable pageable, boolean checked, AplictStatus status) {
-        String jpql = "SELECT ap FROM Aplict ap JOIN FETCH ap.profile p" +
-                " WHERE ap.club.id = :clubId AND ap.checked = :checked AND ap.aplictStatus = :status";
-        TypedQuery<Aplict> query = em.createQuery(jpql, Aplict.class);
-
-        query.setParameter("clubId", clubId);
-        query.setParameter("checked", checked);
-        query.setParameter("status", status);
-
-        query.setFirstResult((int) pageable.getOffset());
-        query.setMaxResults(pageable.getPageSize());
-
-        List<Aplict> resultList = query.getResultList();
-
-        long total = getFailedAplictTotalCount(clubId, checked, status);
-
-        return new PageImpl<>(resultList, pageable, total);
-    }
-
-    private long getAplictTotalCount(Long clubId, boolean checked) {
-        String countJpql = "SELECT COUNT(ap) FROM Aplict ap" +
-                " WHERE ap.club.id = :clubId AND ap.checked = :checked";
-        return em.createQuery(countJpql, Long.class)
-                .setParameter("clubId", clubId)
-                .setParameter("checked",checked)
-                .getSingleResult();
-    }
-
-    private long getFailedAplictTotalCount(Long clubId, boolean checked, AplictStatus status) {
-        String countJpql = "SELECT COUNT(ap) FROM Aplict ap" +
-                " WHERE ap.club.id = :clubId AND ap.checked = :checked AND ap.aplictStatus = :status";
-        return em.createQuery(countJpql, Long.class)
-                .setParameter("clubId", clubId)
+    public List<Aplict> findAllWithProfileByClubIdAndFailed(Long clubId, boolean checked, AplictStatus status) {
+        return em.createQuery(
+                        "SELECT ap FROM Aplict ap JOIN FETCH ap.profile p" +
+                                " WHERE ap.club.id = :clubId AND ap.checked = :checked AND ap.aplictStatus = :status",
+                        Aplict.class
+                ).setParameter("clubId", clubId)
                 .setParameter("checked", checked)
                 .setParameter("status", status)
-                .getSingleResult();
+                .getResultList();
     }
 
 }
