9aa28768d1b5f3f3d6f903392330e4d0102c4977
Hyeok <bh1848@naver.com>
2025-02-15 01:35:52 +0900

    fix: aplictId로 db 조회

--- src/main/java/com/USWCicrcleLink/server/aplict/repository/AplictRepository.java
+++ src/main/java/com/USWCicrcleLink/server/aplict/repository/AplictRepository.java
@@ -2,9 +2,12 @@ package com.USWCicrcleLink.server.aplict.repository;
 
 import com.USWCicrcleLink.server.aplict.domain.Aplict;
 import com.USWCicrcleLink.server.aplict.domain.AplictStatus;
+import com.USWCicrcleLink.server.club.club.domain.Club;
 import com.USWCicrcleLink.server.profile.domain.Profile;
 import org.checkerframework.checker.units.qual.A;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,8 +16,6 @@ import java.util.UUID;
 
 public interface AplictRepository extends JpaRepository<Aplict, Long> ,AplictRepositoryCustom{
     List<Aplict> findByProfileProfileId(Long profileId);
-
-    Optional<Aplict> findByAplictUUID(UUID aplictUUID);
 
     Optional<Aplict> findByClub_ClubIdAndAplictUUIDAndChecked(Long clubId, UUID aplictUUID, boolean checked);
 
@@ -27,4 +28,7 @@ public interface AplictRepository extends JpaRepository<Aplict, Long> ,AplictRep
 
     // AplictRepository에서 지원 여부 확인
     boolean existsByProfileAndClub_ClubId(Profile profile, Long clubId);
+
+    @Query("SELECT a.club FROM Aplict a WHERE a.aplictId = :aplictId")
+    Optional<Club> findClubByAplictId(@Param("aplictId") Long aplictId);
 }
