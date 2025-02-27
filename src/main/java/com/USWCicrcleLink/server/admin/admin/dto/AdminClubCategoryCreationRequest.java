69a6ec7d7ccb997d059a880b5fe2ee17b4cd0118
Hyeok <bh1848@naver.com>
2025-02-27 22:31:28 +0900

    fix: 제약 조건 수정

--- src/main/java/com/USWCicrcleLink/server/admin/admin/dto/AdminClubCategoryCreationRequest.java
+++ src/main/java/com/USWCicrcleLink/server/admin/admin/dto/AdminClubCategoryCreationRequest.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class AdminClubCategoryCreationRequest {
 
     @NotBlank(message = "카테고리 이름은 필수 입력 값입니다.", groups = ValidationGroups.NotBlankGroup.class)
-    @Size(min = 1, max = 20, message = "카테고리는 1~20자 이내여야 합니다.", groups = ValidationGroups.SizeGroup.class)
+    @Size(max = 20, message = "카테고리는 1~20자 이내여야 합니다.", groups = ValidationGroups.SizeGroup.class)
     @Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "카테고리에는 공백 또는 특수문자를 포함할 수 없습니다.", groups = ValidationGroups.PatternGroup.class)
     private String clubCategoryName;
 }
