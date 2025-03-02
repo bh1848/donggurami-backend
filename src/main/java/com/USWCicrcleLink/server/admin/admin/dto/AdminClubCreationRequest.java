37d9650fe025708484d79bb393da38f108f9c8ba
namgoong <cookie_31@naver.com>
2025-03-02 17:02:20 +0900

    fix: AadminClubCreationRequest lederPw regexp

--- src/main/java/com/USWCicrcleLink/server/admin/admin/dto/AdminClubCreationRequest.java
+++ src/main/java/com/USWCicrcleLink/server/admin/admin/dto/AdminClubCreationRequest.java
@@ -26,8 +26,8 @@ public class AdminClubCreationRequest {
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.", groups = ValidationGroups.NotBlankGroup.class)
     @Size(min = 8, max = 20, message = "비밀번호는 8~20자 이내여야 합니다.", groups = ValidationGroups.SizeGroup.class)
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])(?!.*\\s).*$",
-            message = "비밀번호는 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 하며 공백을 포함할 수 없습니다.",
+            regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])(?!.*\\s).*$",
+            message = "비밀번호는 영문, 숫자, 특수문자를 모두 포함해야 하며 공백을 포함할 수 없습니다.",
             groups = ValidationGroups.PatternGroup.class
     )
     private String leaderPw;
